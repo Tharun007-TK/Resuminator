24e73a720365c49f0880c032bdbb47e0350364f6
Tharun Kumar <tharunkumarvmt@gmail.com>
2025-04-24 22:41:49 +0530

    Update client.ts
    client.ts changed with supabase creds

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,15 +1,7 @@
-import { createBrowserClient } from "@supabase/ssr"
-import type { Database } from "./database.types"
+import { createBrowserClient } from "@supabase/ssr";
 
-let supabaseClient: ReturnType<typeof createBrowserClient<Database>> | null = null
-
-export function createClientSupabaseClient() {
-  if (supabaseClient === null) {
-    supabaseClient = createBrowserClient<Database>(
-      process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    )
-  }
-
-  return supabaseClient
-}
+export const createClient = () =>
+  createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  );
